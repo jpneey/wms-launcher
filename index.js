3da5e3b374d0d0f72e77e67adb5d6e2555002672
jpneey <burato348@gmail.com>
2021-06-08 15:49:20 +0800

    Feat: completed v1

--- index.js
+++ index.js
@@ -1,38 +1,50 @@
 const path = require('path')
+const config = require(path.join(__dirname, '..\\extraResources\\config.json'))
 const electron = require('electron')
 const { app, BrowserWindow, ipcMain } = electron
 const shutdown = require('electron-shutdown-command')
-const root = 'http://192.168.101.88/wms/'
-const prel = path.join(__dirname, 'preload.js')
+const root = config.root
+const preload = path.join(__dirname, 'preload.js')
 
+let param = {}
 
-app.on('ready', () => {
-  let window = new BrowserWindow({
+if (config.kiosk) {
+  param = {
     resizable: false,
     fullscreen: true,
     minimizable: false,
     frame: false,
     webPreferences: {
-      preload: prel,
+      preload,
       enableRemoteModule: true,
       contextIsolation: false
     }
-  })
-  window.loadURL(root, {"extraHeaders" : "pragma: no-cache\n"})
+  }
+} else {
+  param = {
+    minWidth: 1280,
+    minHeight: 720
+  }
+}
+
+app.on('ready', () => {
+  let window = new BrowserWindow(param)
+  window.loadURL(root)
   background(window)
 })
 
 const background = (window) => {
+  if (!config.kiosk) {
+    window.maximize()
+    window.setMenuBarVisibility(false)
+  }
+
   ipcMain.on('app-exit', (event, msg) => {
-    // should shutdown
-    // shutdown.shutdown();
-    window.close()
+    shutdown.shutdown();
   })
 
   ipcMain.on('app-reboot', (event, msg) => {
-    // should reboot
-    // shutdown.reboot();
-    window.close()
+    shutdown.reboot();
   })
 
   ipcMain.on('app-minimize', (event, msg) => {
